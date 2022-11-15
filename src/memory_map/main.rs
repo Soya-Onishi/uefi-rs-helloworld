0113189519bf9fc2bb2bc254a65c2d59f38a1789
SoyaOhnishi <soya.ohnishi@gmail.com>
2022-11-15 23:46:31 +0900

    メモリマップの１つ目のエントリの各バイトの値を表示するプログラムの作成 各メモリエントリの見やすい表示の実装などは後ほど

---
+++ src/memory_map/main.rs
@@ -0,0 +1,42 @@
+#![no_std]
+#![no_main]
+#![feature(abi_efiapi)]
+
+use log::info;
+use uefi::{prelude::*, alloc::exit_boot_services, table::boot::MemoryMapKey};
+use uefi::table::boot::MemoryType;
+use uefi::Result;
+
+#[entry]
+fn main(image_handle: Handle, mut system_table: SystemTable<Boot>) -> Status {
+    uefi_services::init(&mut system_table).unwrap();
+    let boot_services = system_table.boot_services();
+    print_memory_map(&boot_services).unwrap();
+
+    boot_services.stall(10_000_000);
+
+    Status::SUCCESS
+}
+
+fn print_memory_map(boot_services: &BootServices) -> Result {
+    let memmap_size = boot_services.memory_map_size();
+    let entry_nums = (memmap_size.map_size + memmap_size.entry_size - 1) / memmap_size.entry_size;
+  
+    // メモリマップを格納するためのバッファサイズを計算する
+    // 現状のエントリが入る分だけだと操作の過程で溢れる可能性があるため、
+    // エントリを余計にとる（EXTRA_ENTRY_NUM）
+    let extra_entry_num = 8;
+    let buffer_size = (entry_nums + extra_entry_num) * memmap_size.entry_size;
+
+    // メモリマップを格納するためのバッファを確保する
+    // 確保したバッファにメモリマップを格納する
+    let memmap_buf = boot_services.allocate_pool(MemoryType::BOOT_SERVICES_DATA, buffer_size)?;
+    let memmap_buf = unsafe { core::slice::from_raw_parts_mut(memmap_buf, buffer_size) };
+    let (key, _) = boot_services.memory_map(memmap_buf)?;
+
+    for idx in 0..memmap_size.entry_size {
+        info!("{}", memmap_buf[idx]);
+    }
+
+    Ok(())
+}
